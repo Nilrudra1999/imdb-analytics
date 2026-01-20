a232772752c81cb6264f62ffa4ec8b3849ab6cdf
Nilrudra1999 <nilrudram@gmail.com>
2026-01-20 00:23:35 -0500

    movie analytics - sql scripts

---
+++ SQL scripts/movie_data_time_correlations.sql
@@ -0,0 +1,30 @@
+/******************************************************************************************
+		IMDd Analytics project - correlation query script - movie elements vs time
+-------------------------------------------------------------------------------------------
+Author: Nilrudra Mukhopadhyay
+Email: nilrudram@gmail.com
+-------------------------------------------------------------------------------------------
+Looking at the way movies and their related metrics changed over a duration of time.
+The related metrics in this case are but not limited to runtime, country of origin, 
+total budgets, domestic and international revenues, median ratings, and frequency of 
+movies made per country of origin.
+******************************************************************************************/
+use imdb_analytics_database;
+go
+
+
+-- How have movie runtimes changed over the last decade?
+-- How have movie revenues changed over the last decade?
+-- How has total budgets changed over the last decade?
+-- How has the ratio of domestic to international profits changed over the decade?
+-- How has the profit per min of runtime changed over the last decade?
+
+-- How have movie genres changed over the last decade?
+-- How has the total number of movies per genre, per year changed over the decade?
+-- How has the median ratings for movies changed over the last decade?
+
+-- How have the countries of origin changed over the last decade?
+-- How has the total number of movies per country, per year changed over the decade?
+
+-- Which directors have made movies over the past decade?
+-- Which directors have made the most movies per year over the last decade?
