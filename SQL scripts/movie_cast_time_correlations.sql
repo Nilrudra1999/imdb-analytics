a650a03c187d228e2eda84c022244534220883c8
Nilrudra1999 <nilrudram@gmail.com>
2026-01-20 21:50:25 -0500

    sql scripts - commenting query questions

---
+++ SQL scripts/movie_cast_time_correlations.sql
@@ -0,0 +1,31 @@
+/******************************************************************************************
+		  IMDd Analytics project - correlation query script - movie cast vs time
+-------------------------------------------------------------------------------------------
+Author: Nilrudra Mukhopadhyay
+Email: nilrudram@gmail.com
+-------------------------------------------------------------------------------------------
+Looking at how movie casts have changed over the years and how this change has affected
+movie ratings, awards, and nominations which are historically used to measure the 
+success of movies.
+******************************************************************************************/
+use imdb_analytics_database;
+go
+
+
+-- how many movies have directors made over the decade?
+-- how many movies have actors stared in over the decade?
+-- how many movies have writers written over the decade?
+
+-- what is the average movie rating per director over the decade?
+-- what is the average movie rating per actor over the decade?
+-- what is the average movie rating per writer over the decade?
+
+-- what is the total number of times each actor has worked with each director
+-- what is the total number of times each writer has worked with each director
+-- what is the total number of times each writer has worked with each actor
+-- what is the total number of awards each director's movie has won per year?
+-- what is the total number of nominations each director's movie has won per year?
+
+-- what is the average annual movie ratings for each actor director pair
+-- what is the average annual movie ratings for each writer director pair
+-- what is the average annual movie ratings for each writer actor pair
--- SQL scripts/movie_data_time_correlations.sql
+++ SQL scripts/movie_data_time_correlations.sql
@@ -1,30 +1,37 @@
 /******************************************************************************************
-		IMDd Analytics project - correlation query script - movie elements vs time
+		  IMDd Analytics project - correlation query script - movie data vs time
 -------------------------------------------------------------------------------------------
 Author: Nilrudra Mukhopadhyay
 Email: nilrudram@gmail.com
 -------------------------------------------------------------------------------------------
-Looking at the way movies and their related metrics changed over a duration of time.
-The related metrics in this case are but not limited to runtime, country of origin, 
-total budgets, domestic and international revenues, median ratings, and frequency of 
-movies made per country of origin.
+Looking at the way movies and their quantitative metrics changed over time. The metrics 
+in this case are, but not limited to runtime, country of origin, budgets, domestic and 
+international revenues, median ratings, and frequency of movies made per country.
 ******************************************************************************************/
 use imdb_analytics_database;
 go
 
 
--- How have movie runtimes changed over the last decade?
--- How have movie revenues changed over the last decade?
--- How has total budgets changed over the last decade?
--- How has the ratio of domestic to international profits changed over the decade?
--- How has the profit per min of runtime changed over the last decade?
+-- what are movie revenues over the last decade?
+-- what are the movie budgets over the last decade?
+-- what are movie profits over the last decade?
+-- how has domestic profits changed?
+-- how has international profits changed?
+-- how has the ratio of domestic to international profit changed?
 
--- How have movie genres changed over the last decade?
--- How has the total number of movies per genre, per year changed over the decade?
--- How has the median ratings for movies changed over the last decade?
+-- what are movie runtimes over the last decade?
+-- how has profit per min of runtime changed over the decade?
+-- how has budget per min of runtime changed?
 
--- How have the countries of origin changed over the last decade?
--- How has the total number of movies per country, per year changed over the decade?
+-- how has profits per movie genre changed?
+-- how has the ratio of domestic to international profit per movie genre changed?
 
--- Which directors have made movies over the past decade?
--- Which directors have made the most movies per year over the last decade?
+-- what are the total annual movie profits per country?
+-- what are the total annual movie budgets per country?
+-- what are the total annual genres of movies produced per country?
+
+-- how has average movie ratings changed over the decade?
+-- how has average movie ratings per genre changed?
+-- how has the ratio of movie awards won to nominations changed?
+-- how has the ratio of movie awards won to nominations changed per genre?
+-- how has total number of movies each country makes changed?
